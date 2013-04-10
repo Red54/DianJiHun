8214eec27c6559e5a0d5d9d4902ae887d4669933
謝致邦 <Yeking@Red54.com>
2013-04-10 21:30:48 +0800

    Second release.

---
+++ wp-content/install.php
@@ -0,0 +1,19 @@
+<?php function wp_install_defaults($user_id) {
+	global $wpdb, $wp_rewrite, $current_site, $table_prefix;
+
+	$djh_plugins = array (
+		0 => 'buddypress/bp-loader.php',
+		1 => 'collabpress/cp-loader.php',
+	);
+
+	foreach ($djh_plugins as $plugin) {
+		$current = get_option( 'active_plugins' );
+		$plugin = plugin_basename( trim( $plugin ) );
+		$current[] = $plugin;
+		sort( $current );
+		do_action( 'activate_plugin', trim( $plugin ) );
+		update_option( 'active_plugins', $current );
+	}
+
+	echo "<br>电机魂开源群件安装成功！<br>";
+}
